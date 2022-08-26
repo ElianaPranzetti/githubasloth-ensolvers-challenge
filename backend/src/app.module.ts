7a1d2738e0f888d7226708e3c7f53f356a7d472c
asloth <sbenelramirez@gmail.com>
2022-08-25 23:28:13 -0500

    fix: add config module for env variables

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,8 +3,17 @@ import { NoteModule } from './note/note.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [NoteModule, AuthModule, PrismaModule, UserModule],
+  imports: [
+    NoteModule,
+    AuthModule,
+    PrismaModule,
+    UserModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule { }
